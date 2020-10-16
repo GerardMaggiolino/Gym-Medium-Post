48b512fd8f3616bb24b4c20695266ba8efcc6387
Gerard Maggiolino <gerardmaggiolino@gmail.com>
2020-10-15 22:40:15 -0700

    Added other dependencies for agent and rendering.

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup
 setup(
     name="simple_driving",
     version='0.0.1',
-    install_requires=['gym', 'pybullet', 'numpy']
+    install_requires=['gym',
+                      'pybullet',
+                      'numpy',
+                      'matplotlib',
+                      'torch']
 )
