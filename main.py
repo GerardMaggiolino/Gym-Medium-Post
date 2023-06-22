8fd018db50987c7be275d4fd687b38ddc169fae5
noahjt <noah.jthomas@icloud.com>
2023-06-22 16:26:34 +0100

    asd

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def main():
                              torch.nn.Linear(64, 2))
     agent = TRPOAgent(policy=nn)
 
-    #agent.load_model("agent.pth")
+    #agent.load_model("train_one.pth")
     agent.train("SimpleDriving-v0", seed=0, batch_size=5000, iterations=100,
                 max_episode_length=250, verbose=True)
-    agent.save_model("train_one.pth")
+    agent.save_model("windows.pth")
 
     env = gym.make('SimpleDriving-v0')
     ob = env.reset()
